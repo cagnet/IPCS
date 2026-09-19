af4b20470c12aceb279063dc924867a4e6419c40
Cyril Cagnet (External) <E-cyril.cagnet@aglgroup.com>
2026-09-19 10:40:15 +0200

    feat: add transformer and fuse components

--- src/model/components.js
+++ src/model/components.js
@@ -4,5 +4,7 @@ export class Coil extends Component{constructor(d={}){super({...d,type:'coil',te
 export class Contact extends Component{constructor(d={}){super({...d,type:'contact',terminals:[{id:'A'},{id:'B'},{id:'COM'},{id:'NC'},{id:'NO'}]});this.contactType=d.contactType||'NO';this.controlType=d.controlType||'manual';this.controllerId=d.controllerId||null;this.manualActivated=!!d.manualActivated;}}
 export class Lamp extends Component{constructor(d={}){super({...d,type:'lamp',terminals:d.terminals||[{id:'A'},{id:'B'}]});this.number=d.number||'';}}
 export class Motor extends Component{constructor(d={}){super({...d,type:'motor',terminals:d.terminals||[{id:'A'},{id:'B'}]});}}
+export class Transformer extends Component{constructor(d={}){super({...d,type:'transformer',terminals:[{id:'P1'},{id:'P2'},{id:'S1'},{id:'S2'},{id:'S3'}]});}}
+export class Fuse extends Component{constructor(d={}){super({...d,type:'fuse',terminals:d.terminals||[{id:'A'},{id:'B'}]});this.rating=d.rating||'10 A';this.blown=!!d.blown;}}
 export class Junction extends Component{constructor(d={}){super({...d,type:'junction',terminals:d.terminals||[{id:'J'}]});}}
-export const componentFromData=d=>({powerSource:PowerSource,coil:Coil,contact:Contact,lamp:Lamp,motor:Motor,junction:Junction}[d.type]||Component)===Component?new Component(d):new ({powerSource:PowerSource,coil:Coil,contact:Contact,lamp:Lamp,motor:Motor,junction:Junction}[d.type])(d);
+export const componentFromData=d=>({powerSource:PowerSource,coil:Coil,contact:Contact,lamp:Lamp,motor:Motor,transformer:Transformer,fuse:Fuse,junction:Junction}[d.type]||Component)===Component?new Component(d):new ({powerSource:PowerSource,coil:Coil,contact:Contact,lamp:Lamp,motor:Motor,transformer:Transformer,fuse:Fuse,junction:Junction}[d.type])(d);
