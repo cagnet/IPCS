da554c2c6bd4b6b364ddb6dbe942343bb124b847
Cyril Cagnet (External) <E-cyril.cagnet@aglgroup.com>
2026-09-19 11:29:48 +0200

    fix: align transformer and restore contact animation

--- src/model/components.js
+++ src/model/components.js
@@ -4,7 +4,7 @@ export class Coil extends Component{constructor(d={}){super({...d,type:'coil',te
 export class Contact extends Component{constructor(d={}){super({...d,type:'contact',terminals:[{id:'A'},{id:'B'},{id:'COM'},{id:'NC'},{id:'NO'}]});this.contactType=d.contactType||'NO';this.controlType=d.controlType||'manual';this.controllerId=d.controllerId||null;this.manualActivated=!!d.manualActivated;}}
 export class Lamp extends Component{constructor(d={}){super({...d,type:'lamp',terminals:d.terminals||[{id:'A'},{id:'B'}]});this.number=d.number||'';}}
 export class Motor extends Component{constructor(d={}){super({...d,type:'motor',terminals:d.terminals||[{id:'A'},{id:'B'}]});}}
-export class Transformer extends Component{constructor(d={}){super({...d,type:'transformer',terminals:[{id:'P1'},{id:'P2'},{id:'S1'},{id:'S2'},{id:'S3'}]});}}
+export class Transformer extends Component{constructor(d={}){super({...d,type:'transformer',terminals:[{id:'P1',kind:'primary'},{id:'P2',kind:'primary'},{id:'S1',kind:'phase'},{id:'S2',kind:'neutral'},{id:'S3',kind:'phase'}]});this.heightCells=Math.max(10,d.heightCells||10);}}
 export class Fuse extends Component{constructor(d={}){super({...d,type:'fuse',terminals:d.terminals||[{id:'A'},{id:'B'}]});this.rating=d.rating||'10 A';this.blown=!!d.blown;}}
 export class Junction extends Component{constructor(d={}){super({...d,type:'junction',terminals:d.terminals||[{id:'J'}]});}}
 export const componentFromData=d=>({powerSource:PowerSource,coil:Coil,contact:Contact,lamp:Lamp,motor:Motor,transformer:Transformer,fuse:Fuse,junction:Junction}[d.type]||Component)===Component?new Component(d):new ({powerSource:PowerSource,coil:Coil,contact:Contact,lamp:Lamp,motor:Motor,transformer:Transformer,fuse:Fuse,junction:Junction}[d.type])(d);
