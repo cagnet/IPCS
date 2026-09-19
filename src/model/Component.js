42575f6789b2bec68314d50f2d8105741b57bc30
Cyril Cagnet (External) <E-cyril.cagnet@aglgroup.com>
2026-09-19 15:06:07 +0200

    feat: improve relay naming and component orientation

--- src/model/Component.js
+++ src/model/Component.js
@@ -1,4 +1,4 @@
 export class Component {
-  constructor({id,type,label=id,position={x:0,y:0},rotation=0,terminals=[]}){Object.assign(this,{id,type,label,position,rotation,terminals});}
+  constructor({id,type,label=id,position={x:0,y:0},rotation=270,terminals=[]}){Object.assign(this,{id,type,label,position,rotation,terminals});}
   terminal(id){return `${this.id}:${id}`;}
 }
