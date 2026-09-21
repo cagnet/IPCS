0d53f6468a1adc1cdaf32ec9f9cbf723541f05e8
Cyril Cagnet (External) <E-cyril.cagnet@aglgroup.com>
2026-09-21 12:24:50 +0200

    feat: revise component labels and contact symbols

--- src/model/Component.js
+++ src/model/Component.js
@@ -1,4 +1,4 @@
 export class Component {
-  constructor({id,type,label=id,position={x:0,y:0},rotation=270,terminals=[],showId,showLabel}){Object.assign(this,{id,type,label,position,rotation,terminals,showId:showId??type!=='junction',showLabel:showLabel??true});}
+  constructor({id,type,label=id,position={x:0,y:0},rotation=270,terminals=[],showId,showLabel}){Object.assign(this,{id,type,label,position,rotation,terminals,showId:showId??false,showLabel:showLabel??true});}
   terminal(id){return `${this.id}:${id}`;}
 }
