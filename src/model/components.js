ea2367b5e7183d1d928afb8012b9054309cdedab
Cyril Cagnet (External) <E-cyril.cagnet@aglgroup.com>
2026-09-26 18:06:34 +0200

    fix: align ADJ orientations and relay type display

--- src/model/components.js
+++ src/model/components.js
@@ -1,6 +1,6 @@
 import {Component} from './Component.js';
 export class PowerSource extends Component{constructor(d={}){super({...d,type:'powerSource',terminals:d.terminals||[{id:'OUT',kind:'output'},{id:'RETURN',kind:'return'}]});this.outputs=d.outputs||[{id:'OUT',label:'24 VAC',voltage:24,enabled:true}];}}
-export class Coil extends Component{constructor(d={}){super({...d,type:'coil',terminals:d.terminals||[{id:'A'},{id:'B'}]});this.relayType=d.relayType||'';}}
+export class Coil extends Component{constructor(d={}){super({...d,type:'coil',terminals:d.terminals||[{id:'A'},{id:'B'}]});this.relayType=d.relayType||'';this.showRelayType=d.showRelayType??true;}}
 export class Contact extends Component{constructor(d={}){super({...d,type:'contact',terminals:[{id:'A'},{id:'B'},{id:'COM'},{id:'NC'},{id:'NO'}]});this.contactType=d.contactType||'NO';this.controllerId=d.controllerId||null;this.controlMode=this.controllerId?'RE':d.controlMode==='CT'||String(d.id||'').startsWith('CT')?'CT':'SW';this.manualActivated=!!d.manualActivated;this.mirrored=!!d.mirrored;this.supplyMirrored=!!d.supplyMirrored;this.commonAlignment=d.commonAlignment==='NO'?'NO':'NC';}}
 export class Lamp extends Component{constructor(d={}){super({...d,type:'lamp',terminals:d.terminals||[{id:'A'},{id:'B'}]});}}
 export class Motor extends Component{constructor(d={}){super({...d,type:'motor',terminals:d.terminals||[{id:'A'},{id:'B'}]});}}
